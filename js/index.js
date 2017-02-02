c188d1ca9758f260b8ddfbcaf4e0badaba723255
Fish_She <1742996017@qq.com>
2017-02-02 16:27:52 +0800

    提交项目

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('body').scrollspy({ target: '.navbar-fixed-top' });
+$(function() {
+    $('body').on('click', 'a.scrollable', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({ scrollTop: $($anchor.attr('href')).offset().top }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
